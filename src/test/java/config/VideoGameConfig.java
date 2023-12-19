9812b7bcb41fba1c47f8faf20ebba25d385a6081
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-12-19 12:55:16 +0530

    Changed config name

--- src/test/java/config/VideoGameConfig.java
+++ src/test/java/config/VideoGameConfig.java
@@ -2,19 +2,23 @@ package config;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import org.junit.BeforeClass;
 
-public class TestConfig {
+public class VideoGameConfig {
 
     @BeforeClass
     public static void setup() {
-        RestAssured.baseURI = "https://videogamedb.uk/";
-        RestAssured.basePath = "api/v2";
-        RestAssured.port = 443;
 
         RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://videogamedb.uk/")
+                .setBasePath("/api/v2")
                 .setContentType("application/json")
                 .addHeader("Accept", "application/json")
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
                 .build();
     }
 }
