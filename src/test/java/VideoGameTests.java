986bbe055f6cbc4ffe38519e76e28183761436bb
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-12-20 16:43:40 +0530

    Added two new PUT test cases

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -6,6 +6,23 @@ import static io.restassured.RestAssured.given;
 
 public class VideoGameTests extends VideoGameConfig {
 
+    String gameJSONBody = "{\n" +
+            "  \"category\": \"Platform\",\n" +
+            "  \"name\": \"God of war 4\",\n" +
+            "  \"rating\": \"Universal\",\n" +
+            "  \"releaseDate\": \"2018-05-04\",\n" +
+            "  \"reviewScore\": 95\n" +
+            "}";
+
+    String gameXMLBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<VideoGameRequest>\n" +
+            "\t<category>Platform</category>\n" +
+            "\t<name>RDR2</name>\n" +
+            "\t<rating>Mature</rating>\n" +
+            "\t<releaseDate>2016-05-04</releaseDate>\n" +
+            "\t<reviewScore>85</reviewScore>\n" +
+            "</VideoGameRequest>";
+
     @Test
     public void getVideoGames() {
         given()
@@ -16,14 +33,6 @@ public class VideoGameTests extends VideoGameConfig {
 
     @Test
     public void createNewGameWithJSON() {
-        String gameJSONBody = "{\n" +
-                "  \"category\": \"Platform\",\n" +
-                "  \"name\": \"God of war 4\",\n" +
-                "  \"rating\": \"Mature\",\n" +
-                "  \"releaseDate\": \"2018-05-04\",\n" +
-                "  \"reviewScore\": 95\n" +
-                "}";
-
         given()
                 .body(gameJSONBody)
         .when()
@@ -33,21 +42,32 @@ public class VideoGameTests extends VideoGameConfig {
 
     @Test
     public void createNewGameWithXML() {
-        String gameXMLBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<VideoGameRequest>\n" +
-                "\t<category>Platform</category>\n" +
-                "\t<name>RDR2</name>\n" +
-                "\t<rating>Mature</rating>\n" +
-                "\t<releaseDate>2016-05-04</releaseDate>\n" +
-                "\t<reviewScore>85</reviewScore>\n" +
-                "</VideoGameRequest>";
-
         given()
                 .body(gameXMLBody)
                 .contentType("application/xml") // Overriding base content type value
                 .accept("application/json") // Overriding base content type value
         .when()
                 .post(VideoGameEndpoints.ALL_VIDEO_GAMES)
+        .then();
+    }
+
+    @Test
+    public void updateGameByJSON() {
+        given()
+                .body(gameJSONBody)
+        .when()
+                .put("/videogame/2")
+        .then();
+    }
+
+    @Test
+    public void updateGameByXML() {
+        given()
+                .body(gameXMLBody)
+                .header("Content-Type", "application/xml")
+                .header("Accept", "application/xml")
+        .when()
+                .put("/videogame/3")
         .then();
     }
 }
