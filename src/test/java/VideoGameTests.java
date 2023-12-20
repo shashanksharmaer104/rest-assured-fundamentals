59fe14f5602ff405150c1d754c7b0daf6ab95a9d
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-12-20 16:17:21 +0530

    Added new POST XML test method

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -30,4 +30,24 @@ public class VideoGameTests extends VideoGameConfig {
                 .post(VideoGameEndpoints.ALL_VIDEO_GAMES)
         .then();
     }
+
+    @Test
+    public void createNewGameWithXML() {
+        String gameXMLBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<VideoGameRequest>\n" +
+                "\t<category>Platform</category>\n" +
+                "\t<name>RDR2</name>\n" +
+                "\t<rating>Mature</rating>\n" +
+                "\t<releaseDate>2016-05-04</releaseDate>\n" +
+                "\t<reviewScore>85</reviewScore>\n" +
+                "</VideoGameRequest>";
+
+        given()
+                .body(gameXMLBody)
+                .contentType("application/xml") // Overriding base content type value
+                .accept("application/json") // Overriding base content type value
+        .when()
+                .post(VideoGameEndpoints.ALL_VIDEO_GAMES)
+        .then();
+    }
 }
