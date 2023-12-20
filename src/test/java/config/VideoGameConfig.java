4852d3acfb71fc68234fe5292ffebdf9735481aa
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-12-20 15:46:26 +0530

    Add new test class having new get test method

--- src/test/java/config/VideoGameConfig.java
+++ src/test/java/config/VideoGameConfig.java
@@ -3,6 +3,8 @@ package config;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.BeforeClass;
 
 public class VideoGameConfig {
@@ -14,6 +16,8 @@ public class VideoGameConfig {
                 .setBaseUri("https://videogamedb.uk/")
                 .setBasePath("/api/v2")
                 .setContentType("application/json")
+                .addFilter(new RequestLoggingFilter()) // To print all request logs
+                .addFilter(new ResponseLoggingFilter()) // To print all response logs
                 .addHeader("Accept", "application/json")
                 .build();
 
---
+++ src/test/java/config/VideoGameEndpoints.java
@@ -0,0 +1,6 @@
+package config;
+
+public interface VideoGameEndpoints {
+    String ALL_VIDEO_GAMES = "/videogame";
+    String SINGLE_VIDEO_GAME = "/videogame/{videoGameId}";
+}
