1b543ea2633ad63ffee7440911e2a72454c022a1
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-12-28 23:35:53 +0530

    Added new test cases for Gpath

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,6 +2,7 @@ import config.FootballConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.get;
@@ -21,4 +22,20 @@ public class GPathJSONTests extends FootballConfig {
         }
     }
 
+    @Test
+    public void extractSingleValueWithFind() {
+        Response response = get("/teams/57");
+        String singlePlayerName = response.path("squad.find { it.id = 4832 }.name");
+
+        System.out.println("Player name: " + singlePlayerName);
+    }
+
+    @Test
+    public void extractListOfValuesWithFindAll() {
+        Response response = get("/teams/57");
+        List<String> listOFPlayers = response.path("squad.findAll { it.id }.name");
+
+        System.out.println("List of players: " + listOFPlayers);
+        System.out.println("Size of List of players: " + listOFPlayers.size());
+    }
 }
